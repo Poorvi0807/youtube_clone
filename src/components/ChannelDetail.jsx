fc3882e61d603f55deb1fe6407be33a5389ce8e5
Poorvi Singh Thakur <poorvisinghthakur27@gmail.com>
2024-02-23 01:05:28 +0530

    Channel Detail

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import { useState, useEffect } from "react";
+import {  ChannelCard } from "./";
 const ChannelDetail = () => {
+    const [ChannelDetail, setChannelDetail] = useState(null);
   return (
     <div>
-      
+       <ChannelCard ChannelDetail={ChannelDetail} marginTop="-110px" />
     </div>
   )
 }
