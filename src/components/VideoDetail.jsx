7cecc3fa255747eba9ece06040837a57e6a2d3cc
TechSiren <asinha27072002@gmailo.com>
2024-02-23 01:14:29 +0530

    video card details

---
+++ src/components/VideoDetail.jsx
@@ -0,0 +1,5 @@
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { Typography, Box, Stack } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
