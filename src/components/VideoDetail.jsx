f058eb34db17926ac9eec44a6fa9e1c40a570685
TechSiren <asinha27072002@gmailo.com>
2024-02-23 01:14:48 +0530

    video detail implementing

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -3,3 +3,6 @@ import { Link, useParams } from "react-router-dom";
 import ReactPlayer from "react-player";
 import { Typography, Box, Stack } from "@mui/material";
 import { CheckCircle } from "@mui/icons-material";
+
+import { Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
